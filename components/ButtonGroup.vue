259bdca58e6d1ca702e889669395a914de01b47f
Vino Samari <iamvinomusic@gmail.com>
2021-09-18 14:56:41 +0000

    Added watch on youtube link

--- components/ButtonGroup.vue
+++ components/ButtonGroup.vue
@@ -4,8 +4,8 @@
       <label for="vue">
         <input type="radio" name="vueOrNuxt" id="vue" checked value="Vue" />VUE
       </label>
-      <label for="vue">
-        <input type="radio" name="vueOrNuxt" id="Nuxt" value="Nuxt" />NUXT
+      <label for="nuxt">
+        <input type="radio" name="vueOrNuxt" id="nuxt" value="Nuxt" />NUXT
       </label>
     </form>
   </section>
@@ -16,12 +16,12 @@ form {
   @apply flex pt-8 pb-5;
 }
 label {
-  @apply font-black text-five text-lg mx-auto;
+  @apply font-black text-five text-lg mx-auto bg-green-700 px-5 py-2 rounded-3xl;
 }
 input {
-  @apply bg-one text-green-700 p-4 mr-1 focus:outline-none border-0 outline-none;
+  @apply bg-green-700 text-green-700 p-4 mr-1 focus:outline-none border-0 outline-none;
 }
 input:checked:focus {
-  @apply border-0 outline-none;
+  @apply border-0 outline-none ring-0 text-ten;
 }
 </style>
--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,23 @@
         <h4 class="description" slot="description">
           {{ video.snippet.description || "Learn Vue.js Today!" }}
         </h4>
-        <a class="watcher" href="" slot="watch">Watch Video</a>
+        <a
+          class="watcher"
+          :href="`https://www.youtube.com/watch?v=${video.id.videoId}`"
+          slot="watch"
+          target="_blank"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 576 512"
+            height="2rem"
+            fill="#002500"
+          >
+            <path
+              d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"
+            />
+          </svg>
+          Watch Video</a
+        >
       </video-card>
     </section>
   </main>
@@ -33,7 +49,7 @@ export default {
   methods: {
     async fetchFirstTwenty() {
       let ytURL =
-        "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=vue.js&key=AIzaSyA3TBqjc6i7MiMNLjnkY0Q8Cyszn_5coc0";
+        "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=vue.js&key=AIzaSyBFy3TI3-fnyWwap-RViSfUXPFdbRXIgFs";
       let resp = await this.$axios.get(ytURL);
       this.videos = resp.data.items;
       console.log("Fetching Videos...");
@@ -54,7 +70,7 @@ img {
   @apply w-full h-3/4 object-cover;
 }
 .videoCardGroup {
-  @apply grid md:grid-cols-2;
+  @apply grid md:grid-cols-3;
 }
 .title {
   @apply uppercase font-bold text-lg px-4 pt-4;
@@ -66,6 +82,6 @@ img {
   @apply text-one capitalize font-bold tracking-widest;
 }
 .watcher {
-  @apply mb-4 bg-two text-ten uppercase font-bold px-4 py-2 rounded-xl;
+  @apply mb-4 bg-two text-ten uppercase font-bold px-4 py-2 rounded-xl tracking-widest  w-auto text-lg inline-flex items-center gap-x-3;
 }
 </style>
